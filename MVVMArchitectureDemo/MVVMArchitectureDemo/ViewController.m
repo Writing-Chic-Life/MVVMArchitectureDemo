8b278fdae7f0e7be5dca6e498b6bc3bd323f3e4c
袁应荣 <yuanyingrong@yuanyingrong.local>
2018-03-23 12:01:42 +0800

    1.创建项目

---
+++ MVVMArchitectureDemo/MVVMArchitectureDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MVVMArchitectureDemo
+//
+//  Created by 袁应荣 on 2018/3/23.
+//  Copyright © 2018年 袁应荣. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
